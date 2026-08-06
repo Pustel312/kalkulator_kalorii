ade1200dddd300297ca0b0965a94d636bf2e0298
Dawid <d,starzec2003@gmail.com>
2026-08-06 06:16:11 +0200

    feat: adding json files

--- src/math_core.py
+++ src/math_core.py
@@ -1,10 +1,40 @@
-baza_produktow = []
+import json
+def load_products():
+    try:
+        with open("baza.json", "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        with open("baza.json", "w", encoding="utf-8") as f:
+            json.dump([], f)
+        return []
+
+def save_products(baza_produktow):
+    with open("baza.json", "w", encoding="utf-8") as f:
+        json.dump(baza_produktow, f, indent=4)
+
+def load_note():
+    try:
+        with open("dziennik.json", "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        with open("dziennik.json", "w", encoding="utf-8") as f:
+            json.dump([], f)
+        return []
+
+def save_note(dziennik_posilkow):
+    with open("dziennik.json", "w", encoding="utf-8") as f:
+        json.dump(dziennik_posilkow, f, indent=4)
+
 def food_menu():
+
+    baza_produktow = load_products()
+    dziennik_posilkow = load_note()
     is_running = True
     while is_running:
         print("\n--- BAZA PRODUKTÓW I POSIŁKI ---")
         print("1. Dodaj nowy produkt do kartoteki")
         print("2. Zaloguj zjedzony posiłek")
+        print("3. Sprawdź bilans dnia")
         print("0. Powrót do menu głównego")
         
         choice = int(input("Wybierz opcję: "))
@@ -18,13 +48,34 @@ def food_menu():
             kalorie = Nowy_produkt["kalorie"]
             print(f"Twój nowo dodany produkt to: {nazwa}, ktory posiada {kalorie} kalorii")
             baza_produktow.append(Nowy_produkt)
+            save_products(baza_produktow)
         elif choice == 2:
             if not baza_produktow:
                 print("Baza jest pusta!")
             else:
                 print("\n--- DOSTĘPNE PRODUKTY ---")
                 for idx, produkt in enumerate(baza_produktow):
-                    print(f"{idx + 1}. {produkt['nazwa']}")
+                    print(f"{idx}. {produkt['nazwa']}")
+                product_idx = int(input("Który produkt chcesz dodać? "))
+                if product_idx in range(0, len(baza_produktow)):
+                    product_choice = baza_produktow[product_idx]
+                    weight_choice = int(input("Jaka jest waga produktu? "))
+                    porcja = calculate_portion(produkt=product_choice, weight=weight_choice)
+                    print(f"Twój dodany log to: {porcja['nazwa']}, która posiada: B: {porcja['bialko']}, T: {porcja['tluszcze']}, W: {porcja['weglowodany']}, ktore sie skladaja na {porcja['kalorie']} kalorii.")
+                    dziennik_posilkow.append(porcja)
+                    save_note(dziennik_posilkow)
+                    
+        elif choice == 3:
+            suma_kalorii = 0
+            suma_bialko = 0
+            suma_tluszcze = 0
+            suma_weglowodany = 0
+            for posilek in dziennik_posilkow:
+                suma_kalorii += posilek['kalorie']
+                suma_bialko += posilek['bialko']
+                suma_tluszcze += posilek['tluszcze']
+                suma_weglowodany += posilek['weglowodany']
+            print(f"Łączna liczba kalorii to: {suma_kalorii}, BTW to: {suma_bialko}, {suma_tluszcze}, {suma_weglowodany}.")
         elif choice == 0:
             is_running = False
 
@@ -66,3 +117,15 @@ def adjust_calories_for_goal(goal: str, tdee: float)->float:
     elif goal == "nadwyzka":
        return tdee + 400.0
     return tdee
+
+def calculate_portion(produkt: dict, weight: float)->dict:
+    conversion_factor = weight/100
+    porcja = {
+            "nazwa": produkt["nazwa"],
+            "waga": weight,
+            "bialko": produkt["bialko"]*conversion_factor,
+            "tluszcze": produkt["tluszcze"]*conversion_factor,
+            "weglowodany": produkt["weglowodany"]*conversion_factor,
+            "kalorie": produkt["kalorie"]*conversion_factor
+        }
+    return porcja
